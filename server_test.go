b04f6f3857eca6d6535f61593af5d6dbd54ef485
Jon Hadfield <jon@lessknown.co.uk>
2020-08-27 21:21:33 +0100

    add tests.

--- server_test.go
+++ server_test.go
@@ -9,8 +9,28 @@ import (
 )
 
 func TestGET(t *testing.T) {
-	t.Run("check / returns ok", func(t *testing.T) {
+	t.Run("check GET / returns ok", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		Server(response, request)
+		assert.Equal(t, "ok", response.Body.String())
+	})
+}
+
+func TestPOST(t *testing.T) {
+	t.Run("check POST / returns ok", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/", nil)
+		response := httptest.NewRecorder()
+
+		Server(response, request)
+		assert.Equal(t, "ok", response.Body.String())
+	})
+}
+
+func TestHEAD(t *testing.T) {
+	t.Run("check HEAD / returns ok", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodHead, "/", nil)
 		response := httptest.NewRecorder()
 
 		Server(response, request)
